a36e4743eebcbc65b4c7c09f1309402162a72fc0
PieterRossouw <pieter@synthesis.co.za>
2018-09-12 11:26:40 +0200

    initial working

---
+++ Quiz-Backend/aws-ses.js
@@ -0,0 +1,55 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.loadFromPath(__dirname + '/cred.json')
+
+var sendResults = function (_html, _text, _subject, _email) {
+
+    // Create sendEmail params 
+    var params = {
+        Destination: { /* required */
+            CcAddresses: [],
+            ToAddresses: [
+                _email,
+            ]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: _html
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: _text
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: _subject
+            }
+        },
+        Source: _email,
+        ReplyToAddresses: [
+            _email,
+        ],
+    };
+
+    // Create the promise and SES service object
+    var sendPromise = new AWS.SES({
+        apiVersion: '2010-12-01'
+    }).sendEmail(params).promise();
+
+    // Handle promise's fulfilled/rejected states
+    sendPromise.then(
+        function (data) {
+            console.log(data.MessageId);
+        }).catch(
+        function (err) {
+            console.error(err, err.stack);
+        });
+}
+
+module.exports = {
+    sendResults
+}
---
+++ Quiz/app.js
@@ -0,0 +1,82 @@
+var app = angular.module('nwuQuiz', ['dndLists']);
+
+angular.module("nwuQuiz")
+    .controller("nwuQuizCtrl", function ($scope, $http) {
+        $scope.hostname = "localhost"
+        $scope.models = {
+            selected: null,
+            lists: {
+                Icons: {
+                    allowedTypes: ['Icon'],
+                    items: []
+                },
+                Names: {
+                    allowedTypes: ['Name'],
+                    items: []
+                },
+                Descriptions: {
+                    allowedTypes: ['Description'],
+                    items: []
+                },
+            }
+        };
+        $scope.email = "";
+
+        function init(){
+            $scope.getModel;
+        }
+
+        init()
+
+        $scope.getModel = function () {
+            $http({
+                method: 'GET',
+                url: `http://${$scope.hostname}:3000/api/frameworkList`
+            }).then(function successCallback(response) {
+                console.log(`Response: ${response}`);
+                if (response.data) {
+                    $scope.models.lists = response.data;
+                }
+            }, function errorCallback(response) {
+                console.log(`Error: ${response}`);
+
+            });
+        }
+
+        function validateEmail(email) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
+        }
+
+        $scope.sendModel = function () {
+            if (!validateEmail($scope.email)) {
+                swal("Invalid email!", `'${$scope.email}' failed email validations!`, "error").then(function () {
+                    document.getElementById("email").focus();
+                })
+            } else {
+
+                $scope.models.email = $scope.email;
+                $http({
+                    url: 'http://localhost:3000/api/frameworkList',
+                    method: "POST",
+                    data: {
+                        'model': $scope.models
+                    }
+                })
+                    .then(function (response) {
+                        // success
+                        var r = response.data;
+
+                        if (r.result === true) {
+                            getModel();
+                            swal("Good job!", r.comment, "success");
+                        } else {
+                            swal("Derp!", r.comment, "warning");
+                        }
+                    })
+            }
+        }
+
+
+
+    });
